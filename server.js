5e63359d8ef941fe59228f9d69362a46ed0c7f92
Tejas Shah <tejashah88@gmail.com>
2017-02-09 21:56:51 -0800

    finding errors

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ var englishify = function(text) {
 	console.log(err);
 })*/
 
-var alexa = require('alexa-utils');
+/*var alexa = require('alexa-utils');
 const PORT = process.env.PORT || 8080;
 
 var app = alexa.app("CalculusBuddy")
@@ -181,4 +181,43 @@ var app = alexa.app("CalculusBuddy")
 	})
 	.host("/solve", PORT, false);
 
-console.log("Server started on port " + PORT);
+console.log("Server started on port " + PORT);*/
+
+var express = require("express");
+var alexa = require("alexa-app");
+
+var PORT = process.env.port || 8080;
+var app = express();
+
+// ALWAYS setup the alexa app and attach it to express before anything else.
+var alexaApp = new alexa.app("test");
+
+alexaApp.express({
+  expressApp: app,
+  router: express.Router(),
+
+  // verifies requests come from amazon alexa. Must be enabled for production.
+  // You can disable this if you're running a dev environment and want to POST
+  // things to test behavior. enabled by default.
+  checkCert: true,
+
+  // sets up a GET route when set to true. This is handy for testing in
+  // development, but not recommended for production. disabled by default
+  debug: false
+});
+
+// now POST calls to /test in express will be handled by the app.request() function
+
+alexaApp.launch(function(request, response) {
+  response.say("You launched the app!");
+});
+
+alexaApp.post = function(request, response, type, exception) {
+  if (exception) {
+    // always turn an exception into a successful response
+    console.log("An error occured: " + exception);
+  }
+};
+
+app.listen(PORT);
+console.log("Listening on port " + PORT + ", try http://localhost:" + PORT + "/test");
